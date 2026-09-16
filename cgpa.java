c6c6de0e0d87a43b98dfd1a5cd44116ae64de5ae
FatimaAnsar12 <sp26-bai-020@cuilahore.edu.pk>
2026-09-16 22:23:37 +0500

    Add files via upload

---
+++ cgpa.java
@@ -0,0 +1,7 @@
+public class Class{
+	public static void main(String args[]){
+	System.out.println("Name:FatimaAnsar");
+	System.out.println("Registration no.:Sp26-bai-020");
+	System.out.println("Cgpa:3.86");
+	}
+}
